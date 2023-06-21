497aa70df31d3a0a1ad635cc0f91402c6a128598
Emirhan Kılıç <emirhan.kilic@ug.bilkent.edu.tr>
2023-06-22 01:06:57 +0300

    creating main folders and foundational JFrame for the application

---
+++ sortingVisualization/src/main/java/appRunner.java
@@ -0,0 +1,5 @@
+public class appRunner {
+	public static void main(String[] args) {
+		mainFrame mainFrameOfApp = new mainFrame();
+	}
+}
---
+++ sortingVisualization/src/main/java/mainFrame.java
@@ -0,0 +1,19 @@
+
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+
+import javax.swing.*;
+
+public class mainFrame {
+
+
+	public mainFrame() {
+		FlatMacDarkLaf.setup();
+
+		JFrame mainFrameOfApp = new JFrame("Sorting GUI");
+		mainFrameOfApp.setSize(1240, 768);
+		mainFrameOfApp.setLocationRelativeTo(null);
+		mainFrameOfApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrameOfApp.getContentPane().add(new JPanel());
+		mainFrameOfApp.setVisible(true);
+	}
+}
